4392ea086dfe7185b6eec12d3cab075f5a7fceb9
François-Xavier Savary <fx.savary@orange.fr>
2024-09-16 23:29:59 +0200

    jest / test-library setup

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '@ckeditor/ckeditor5-react': '<rootDir>/__mocks__/ckeditor5.tsx',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.jest.json',
+      },
+    ],
+    '^.+\\.scss$': 'jest-scss-transform',
+  },
+  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
