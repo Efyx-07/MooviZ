baf58a7d6925ec17e2c3f8a6d2cdfa9ea5dd85c3
François-Xavier Savary <fx.savary@orange.fr>
2024-09-17 02:34:04 +0200

    test KeywordSearchBar

---
+++ src/components/SearchSection/KeywordSearchBar.spec.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import KeywordSearchBar from './KeywordSearchBar';
+import { getMoviesByKeywordWithGenres } from '@/services/movie.service';
+import useMovieStore from '@/stores/MovieStore';
+
+// Mock des dépendances
+jest.mock('@/services/movie.service', () => ({
+  getMoviesByKeywordWithGenres: jest.fn(),
+}));
+jest.mock('@/stores/MovieStore', () => ({
+  __esModule: true,
+  default: () => ({
+    setMoviesData: jest.fn(),
+    addMoviesToAllSearched: jest.fn(),
+  }),
+}));
+
+describe('KeywordSearchBar', () => {
+  const setMovieData = jest.fn();
+  const addMoviesToAllSearched = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  // Test de rendu
+  it('should render the search bar and icon', () => {
+    render(<KeywordSearchBar />);
+    // Vérifie la présence du champ de saisie
+    const inputElement = screen.getByPlaceholderText('Titre du film');
+    expect(inputElement).toBeInTheDocument();
+    // Vérifie la présence de l'icone de recherche
+    const iconElement = screen.getByRole('button');
+    expect(iconElement).toBeInTheDocument();
+  });
+});
