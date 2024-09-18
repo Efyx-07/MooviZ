1807c3129a1aa19cb40943599329f10b942f8d69
François-Xavier Savary <fx.savary@orange.fr>
2024-09-18 12:52:31 +0200

    framer transitions

--- src/components/AnalyseSection/MovieListView.tsx
+++ src/components/AnalyseSection/MovieListView.tsx
@@ -1,5 +1,6 @@
 import './MovieListView.scss';
 import { Movie } from '@/interfaces/movie.interface';
+import { motion } from 'framer-motion';
 
 interface MovieListViewProps {
   movies: Movie[];
@@ -7,13 +8,18 @@ interface MovieListViewProps {
 
 export default function MovieListView({ movies }: MovieListViewProps) {
   return (
-    <div className="movie-list-container">
+    <motion.div
+      className="movie-list-container"
+      initial={{ y: 30, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.5, ease: 'easeOut' }}
+    >
       {movies.map((movie) => (
         <p key={movie.imdbID}>
           {movie.Title} - {movie.Year} - {movie.Type} - {movie.Genre} -
           {movie.imdbRating}
         </p>
       ))}
-    </div>
+    </motion.div>
   );
 }
--- src/components/MovieSection/PosterView.tsx
+++ src/components/MovieSection/PosterView.tsx
@@ -1,6 +1,7 @@
 import './PosterView.scss';
 import { Movie } from '@/interfaces/movie.interface';
 import MovieCard from './MovieCard';
+import { motion } from 'framer-motion';
 
 interface PosterViewProps {
   movies: Movie[];
@@ -8,8 +9,13 @@ interface PosterViewProps {
 
 export default function PosterView({ movies }: PosterViewProps) {
   return (
-    <div className="poster-view">
+    <motion.div
+      className="poster-view"
+      initial={{ y: 30, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.5, ease: 'easeOut' }}
+    >
       <MovieCard movies={movies} />
-    </div>
+    </motion.div>
   );
 }
